4d6ebb7e8cc9fe4be8ac4ef4e5bc19d7f2cb87d6
shenhaoyi <shenhaoyi04@163.com>
2023-09-08 04:20:12 +0800

    完成generator

---
+++ src/codeGenerator.ts
@@ -0,0 +1,15 @@
+import { TransformedNodeType } from "./constant";
+
+export function codeGenerator(node) {
+  switch (node.type) {
+    case TransformedNodeType.PROGRAM:
+      return '"' + node.body.map(codeGenerator).join(' ') + '"';
+    case TransformedNodeType.EXPRESSION_STATEMENT:
+      return codeGenerator(node.expression) + ';'
+    case TransformedNodeType.CALL_EXPRESSION:
+      return node.callee.name + '(' + node.arguments.map(codeGenerator).join(', ') + ')'
+    case TransformedNodeType.NUMBER_LITERAL:
+      return node.value
+    default:
+  }
+}
---
+++ tests/codeGenerator.test.ts
@@ -0,0 +1,69 @@
+import { test, expect } from 'vitest';
+import { codeGenerator } from '../src/codeGenerator';
+import { TRANSFORMED_AST, TransformedNodeType } from '../src/constant';
+
+test("code generator", () => {
+  expect(codeGenerator(TRANSFORMED_AST)).toBe('"add(2, subtract(4, 2));"');
+});
+
+test("two ExpressionStatement code generator", () => {
+
+  const ast = {
+    type: TransformedNodeType.PROGRAM,
+    body: [{
+      type: TransformedNodeType.EXPRESSION_STATEMENT,
+      expression: {
+        type: TransformedNodeType.CALL_EXPRESSION,
+        callee: {
+          type: TransformedNodeType.IDENTIFIER,
+          name: 'add'
+        },
+        arguments: [{
+          type: TransformedNodeType.NUMBER_LITERAL,
+          value: '2'
+        }, {
+          type: TransformedNodeType.CALL_EXPRESSION,
+          callee: {
+            type: TransformedNodeType.IDENTIFIER,
+            name: 'subtract'
+          },
+          arguments: [{
+            type: TransformedNodeType.NUMBER_LITERAL,
+            value: '4'
+          }, {
+            type: TransformedNodeType.NUMBER_LITERAL,
+            value: '2'
+          }]
+        }]
+      }
+    }, {
+      type: TransformedNodeType.EXPRESSION_STATEMENT,
+      expression: {
+        type: TransformedNodeType.CALL_EXPRESSION,
+        callee: {
+          type: TransformedNodeType.IDENTIFIER,
+          name: 'add'
+        },
+        arguments: [{
+          type: TransformedNodeType.NUMBER_LITERAL,
+          value: '1'
+        }, {
+          type: TransformedNodeType.CALL_EXPRESSION,
+          callee: {
+            type: TransformedNodeType.IDENTIFIER,
+            name: 'subtract'
+          },
+          arguments: [{
+            type: TransformedNodeType.NUMBER_LITERAL,
+            value: '2'
+          }, {
+            type: TransformedNodeType.NUMBER_LITERAL,
+            value: '3'
+          }]
+        }]
+      }
+    }]
+  }
+  expect(codeGenerator(ast)).toBe('"add(2, subtract(4, 2)); add(1, subtract(2, 3));"');
+});
+
